85ad2618f7e5ec46d5662d2fe103210308896780
Seagull.Dev <Seagull.Dev@DESKTOP-I30P7GL>
2018-03-08 18:51:47 +0200

    Initial commit

---
+++ Backend/AngularApi/AngularApi/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AngularApi.Controllers
+{
+    [RoutePrefix("api/Home")]
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        [Route("Index")]
+        public async Task<IHttpActionResult> Index()
+        {
+            return Ok("Hello Angular2");
+        }
+    }
+}
